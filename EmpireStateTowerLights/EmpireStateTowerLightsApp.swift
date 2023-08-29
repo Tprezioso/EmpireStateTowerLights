9316bbfe3390ccec3cb3296258b146b72596573b
Thomas <tommyprezioso@gmail.com>
2023-08-29 19:15:40 -0400

    Initial Commit

---
+++ EmpireStateTowerLights/EmpireStateTowerLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpireStateTowerLightsApp.swift
+//  EmpireStateTowerLights
+//
+//  Created by Thomas Prezioso Jr on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct EmpireStateTowerLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
