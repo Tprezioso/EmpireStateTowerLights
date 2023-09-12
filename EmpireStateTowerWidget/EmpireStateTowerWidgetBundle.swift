76c4d2fbf94ff0be7dbc6c7b029c8635eef05807
Thomas <tommyprezioso@gmail.com>
2023-09-12 07:12:04 -0400

    added widget target

---
+++ EmpireStateTowerWidget/EmpireStateTowerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  EmpireStateTowerWidgetBundle.swift
+//  EmpireStateTowerWidget
+//
+//  Created by Thomas Prezioso Jr on 9/11/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct EmpireStateTowerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        EmpireStateTowerWidget()
+    }
+}
