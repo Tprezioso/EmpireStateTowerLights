05bb56ac148f20f74f6b3763fdc0658c7911c171
Thomas <tommyprezioso@gmail.com>
2023-09-27 22:27:20 -0400

    added splash screen

---
+++ EmpireStateTowerLights/View/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  EmpireStateTowerLights
+//
+//  Created by Thomas Prezioso Jr on 9/27/23.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
