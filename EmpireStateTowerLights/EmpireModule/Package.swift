0a3e3cd998106d7b60c1878aae3eea6b05ba588b
Thomas <tommyprezioso@gmail.com>
2023-12-09 09:26:18 -0500

    comment out unused packages

--- EmpireStateTowerLights/EmpireModule/Package.swift
+++ EmpireStateTowerLights/EmpireModule/Package.swift
@@ -24,18 +24,18 @@ let package = Package(
             targets: ["TowerViews"])
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-collections", from: "1.0.4"),
+//        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.0.0"),
+//        .package(url: "https://github.com/apple/swift-collections", from: "1.0.4"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.2.0"),
-        .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
         .package(url: "https://github.com/scinfu/swiftsoup", from: "2.6.1"),
-        .package(url: "https://github.com/airbnb/lottie-ios", from: "4.3.2"),
-        .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.2"),
+//        .package(url: "https://github.com/airbnb/lottie-ios", from: "4.3.2"),
+//        .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
